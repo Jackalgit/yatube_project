103030940a95a82b52d90fda993eccdf526bfa9b
Ivan Gurianov <gurianovia@gmail.com>
2023-01-21 18:49:03 +0300

    добавлено css img models

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    context = {
+        'title': 'Последние обновления на сайте'
+    }
+    return render(request, template, context)
 
 def group_posts(request):
     template = 'posts/group_list.html'
-    return render(request, template)
-
-# def group_posts(request, slug):
-#    return HttpResponse(f'Группа {slug}')
+    context = {
+        'title': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context)
